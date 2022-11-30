736624d5d47e1f62771f3c01557c9095201c1954
Joanna <yl797@cornell.edu>
2022-11-30 02:13:45 -0500

    Complete app.py, update db.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,13 @@
 from db import db
-from db import User
-from db import Comment
-from db import Post
+from db import *
 from flask import Flask
-from flask import request 
+from flask import request
 import json
 import os
+import datetime
 
 app = Flask(__name__)
 db_filename = "spotted.db"
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
@@ -17,6 +15,8 @@ app.config["SQLALCHEMY_ECHO"] = True
 db.init_app(app)
 with app.app_context():
     db.create_all()
+    Game.init_public_game()
+
 
 def success_response(data, code=200):
     """ 
@@ -31,54 +31,237 @@ def failure_response(message, code=404):
     """
     return json.dumps({"error": message}), code
 
+
 @app.route("/")
-def greeting(): 
-  """Endpoint for greeting user by reading from .env file"""
-  return "Welcome to the TITLE. This game is created by... "
+def greeting():
+    """Endpoint for greeting user by reading from .env file"""
+    return "Welcome to" + os.environ["NAME"] + "! This game is created by "\
+        + os.environ["DESIGN"] + ", " + \
+        os.environ["FRONTEND"] + ", " + os.environ["BACKEND"]
+
 
 @app.route("/api/user/")
-def get_all_users(): 
-  """Endpoint to get all users' profiles"""
-  users = []
-  for u in User.query.all(): 
-    users.append(u.serialize_profile())
-  return success_response({"users": users})
+def get_all_users():
+    """Endpoint to get all users' profiles"""
+    users = []
+    for u in User.query.all():
+        users.append(u.serialize_profile())
+    return success_response({"users": users})
+
 
 @app.route("/api/user/", methods=['POST'])
-def create_user(): 
-  """Endpoint to create an user"""
+def create_user():
+    """Endpoint to create an user"""
+    body = json.loads(request.data)
+    name = body.get('name')
+    stats_id = Stats.init_stats()
+    # stats = Stats.query.filter_by(id=stats_id).first()
+    # game = Game.query.filter_by(id=1).first()
+    if name is None:
+        return failure_response("Name field cannot be empty", 400)
+    new_user = User(
+        name=name,
+        university=body.get('university'),
+        grade=body.get('grade'),
+        posts=[],
+        comments=[],
+        friends=[],
+        game=1,
+        stats=stats_id,
+        target_of_card=[],
+        target_card=[]
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return success_response(new_user.serialize_profile(), 201)
 
-@app.route("/api/user/<int:user_id>/")
-def get_user(user_id): 
-  """Endpoint to get a specific user by id"""
 
-@app.route("/api/user/<int:user_id>", methods=['POST'])
-def get_update_user(user_id): 
-  """Endpoint to update about a user"""
+@app.route("/api/user/<int:user_id>/", methods=['DELETE'])
+def delete_user(user_id):
+    """Endpoint to delete a post by id"""
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+    db.session.delete(user)
+    db.session.commit()
+    return success_response(user.serialize_profile())
 
-@app.route("/api/post/<int:user_id>", methods=['POST'])
-def create_post(): 
-  """Endpoint to create a post for a user"""
 
-@app.route("/api/post/<int:user_id>")
-def get_posts(): 
-  """Endpoint to get all posts of a user"""
+@app.route("/api/user/<int:user_id>/profile")
+def get_user_profile(user_id):
+    """Endpoint to get a specific user's profile by id"""
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+    return success_response(user.serialize_profile())
+
+
+@app.route("/api/user/<int:user_id>/game")
+def get_user_game(user_id):
+    """Endpoint to get a specific user's game information by id"""
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+    return success_response(user.serialize_game())
+
+
+@app.route("/api/user/<int:user_id>/profile", methods=['POST'])
+def get_update_user_profile(user_id):
+    """Endpoint to update user profile"""
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    body = json.loads(request.data)
+    user.name = body.get('name', user.name)
+    user.university = body.get('university', user.university)
+    user.grade = body.get('grade', user.grade)
+    db.session.commit()
+    return success_response(user.serialize_profile())
+
+
+@app.route("/api/user/<int:user_id>/friend/", methods=['POST'])
+def get_update_user_friend(user_id):
+    """Endpoint to add friend to an user"""
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    body = json.loads(request.data)
+    friend_id = body.get('friend_id')
+
+    friend = User.query.filter_by(id=friend_id).first()
+    if friend is None:
+        return failure_response("Friend not found! ")
+
+    user.friends.append(friend)
+    db.session.commit()
+    return success_response(user.serialize_friends())
+
+
+@app.route("/api/user/<int:user_id>/game/", methods=['POST'])
+def get_update_user_game(user_id):
+    """Endpoint to change game for an user"""
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    body = json.loads(request.data)
+    game_id = body.get('game_id')
+
+    game = User.query.filter_by(id=game_id).first()
+    if game is None:
+        return failure_response("Game not found! ")
+    user.game = game_id
+    user.target_card = []
+    user.target_of_card = []
+    db.session.commit()
+    return success_response(user.serialize_game())
+
+
+@app.route("/api/post/<int:user_id>/", methods=['POST'])
+def create_post(user_id):
+    """Endpoint to create a post for a user"""
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    body = json.loads(request.data)
+    title = body.get('title')
+    if title is None:
+        return failure_response("Title not inputted", 400)
+
+    contents = body.get('contents')
+    if contents is None:
+        return failure_response("Contents not inputted", 400)
+
+    new_post = Post(
+        user=user_id,
+        title=title,
+        contents=contents,
+        comments=[],
+        time=str(datetime.datetime.now())
+    )
+
+    db.session.add(new_post)
+    db.session.commit()
+    return success_response(new_post.serialize())
+
+
+@app.route("/api/post/<int:user_id>/")
+def get_posts(user_id):
+    """Endpoint to get all posts of a user"""
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    the_posts = []
+    for p in Post.query.filter_by(user=user_id):
+        the_posts.append(p.serialize())
+    return success_response({"posts": the_posts})
+
 
 @app.route("/api/post/<int:post_id>/", methods=['DELETE'])
-def delete_post(post_id): 
-  """Endpoint to delete a post by id"""
+def delete_post(post_id):
+    """Endpoint to delete a post by id"""
+    post = Post.query.filter_by(id=post_id).first()
+    if post is None:
+        return failure_response("Post not found!")
+    db.session.delete(post)
+    db.session.commit()
+    return success_response(post.serialize())
+
 
 @app.route("/api/comment/<int:user_id>/<int:post_id>/", methods=['POST'])
-def create_comment(user_id, post_id): 
-  """Endpoint to create a comment for a user"""
+def create_comment(user_id, post_id):
+    """Endpoint to create a comment for a user"""
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    post = Post.query.filter_by(id=post_id).first()
+    if post is None:
+        return failure_response("Post not found!")
+
+    body = json.loads(request.data)
+    contents = body.get('contents')
+    if contents is None:
+        return failure_response("Contents not inputted", 400)
+
+    new_comment = Comment(
+        user=user_id,
+        contents=contents,
+        post=post_id,
+        time=str(datetime.datetime.now())
+    )
+
+    db.session.add(new_comment)
+    db.session.commit()
+    return success_response(new_comment.serialize())
+
 
 @app.route("/api/comment/<int:post_id>/")
-def get_comment(post_id): 
-  """Endpoint to get all comments of a post"""
+def get_comment(post_id):
+    """Endpoint to get all comments of a post"""
+    post = Post.query.filter_by(id=post_id).first()
+    if post is None:
+        return failure_response("Post not found!")
+
+    the_comments = []
+    for c in Comment.query.filter_by(post=post_id):
+        the_comments.append(c.serialize())
+    return success_response({"comments": the_comments})
+
 
 @app.route("/api/comment/<int:comment_id>/", methods=['DELETE'])
-def delete_comment(comment_id): 
-  """Endpoint to delete a comment by id"""
+def delete_comment(comment_id):
+    """Endpoint to delete a comment by id"""
+    comment = Comment.query.filter_by(id=comment_id).first()
+    if comment is None:
+        return failure_response("Comment not found!")
+    db.session.delete(comment)
+    db.session.commit()
+    return success_response(comment.serialize())
 
 
 if __name__ == "__main__":
--- backend/db.py
+++ backend/db.py
@@ -14,119 +14,210 @@ db = SQLAlchemy()
 # Login
 # Profile Picture
 
-friendship = db.Table("friendship", db.Model.metadata, 
-db.Column("friendship_id", db.Integer, primary_key = True, autoincrement=True),
-db.Column("main_id", db.Integer, db.ForeignKey("user.id")), 
-db.Column("friend_id", db.Integer, db.ForeignKey("user.id")))
+friendship = db.Table("friendship", db.Model.metadata,
+                      db.Column("friendship_id", db.Integer,
+                                primary_key=True, autoincrement=True),
+                      db.Column("main_id", db.Integer,
+                                db.ForeignKey("user.id")),
+                      db.Column("friend_id", db.Integer, db.ForeignKey("user.id")))
 
-class User(db.Model): 
-  __tablename__ = "user"
-  id = db.Column (db.Integer, primary_key = True, autoincrement=True)
-  name = db.Column(db.String, nullable=False)
-  university = db.Column (db.String)
-  grade = db.Column (db.Integer)
-  spotted = db.Column(db.Integer, nullable= False)
-  card = db.Column(db.Integer, db.ForeignKey("card.id"))
-  comments = db.relationship("Comment", cascade = "delete")
-  posts = db.relationship("Post", cascade= "delete")
-  game = db.Column (db.Integer, db.ForeignKey("game.id"))
-  stats = db.Column (db.Integer, db.ForeignKey("stats.id"))
-  friends = db.relationship("User", 
-  secondary = friendship, 
-  primaryjoin= id == friendship.c.main_id, 
-  secondaryjoin = id == friendship.c.friend_id, 
-  back_populates="main")
-  
-  def serialize_profile(self): 
-    """
-    Serializes the profile of the user. This includes id, name, university, grade
-    """
-    return{
-      "id": self.id, 
-      "name": self.name, 
-      "university": self.university, 
-      "grade": self.grade
-    }
-  
-  def serialize_posts(self): 
-    """
-    Serializes the posts of the user. 
-    """
-    return{
-      "id": self.id, 
-      "posts": [p.serialize() for p in self.posts]
-    }
-   
-class Card(db.Model): 
-  __tablename__ = "card"
-  id = db.Column (db.Integer, primary_key = True, autoincrement=True)
-  game = db.Column (db.Integer, db.ForeignKey("game.id"), nullable=False)
-  target = db.Column(db.Integer, db.ForeignKey("user.id"), nullable= False)
-  belong = db.relationship("User", cascade = "delete")
+target_assoc = db.Table("association_target", db.Model.metadata,
+                        db.Column("user_id", db.Integer,
+                                  db.ForeignKey("user.id")),
+                        db.Column("card_id", db.Integer,
+                                  db.ForeignKey("card.id"))
+                        )
 
-class Game(db.Model): 
-  __tablename__ = "game"
-  id = db.Column (db.Integer, primary_key = True, autoincrement=True)
-  users = db.relationship("User", cascade="delete")
-  cards = db.relationship("Card", cascade = "delete")
-  is_private = db.Column (db.Boolean, nullable=False)
+target_of_assoc = db.Table("association_target_of", db.Model.metadata,
+                           db.Column("user_id", db.Integer,
+                                     db.ForeignKey("user.id")),
+                           db.Column("card_id", db.Integer,
+                                     db.ForeignKey("card.id"))
+                           )
 
 
-class Post (db.Model): 
-  __tablename__ = "post"
-  id = db.Column (db.Integer, primary_key = True, autoincrement=True)
-  user = db.Column (db.Integer, db.ForeignKey("user.id"), nullable=False)
-  time = db.Column (db.String, nullable= False)
-  comments = db.relationship("Comment", cascade="delete")
-  title = db.Column (db.String)
-  contents = db.Column (db.String)
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    university = db.Column(db.String, default="")
+    grade = db.Column(db.Integer, default=0)
+    comments = db.relationship("Comment", cascade="delete")
+    posts = db.relationship("Post", cascade="delete")
+    game = db.Column(db.Integer, db.ForeignKey("game.id"))
+    stats = db.Column(db.Integer, db.ForeignKey("stats.id"))
+    friends = db.relationship("User",
+                              secondary=friendship,
+                              primaryjoin=id == friendship.c.main_id,
+                              secondaryjoin=id == friendship.c.friend_id,
+                              back_populates="friends")
+    target_of_card = db.relationship(
+        "Card", secondary=target_of_assoc, back_populates="belong")
+    target_card = db.relationship(
+        "Card", secondary=target_assoc, back_populates="target")
 
-  def serialize(self):
-    """
-    Serializes the contents of the post
-    """
-    return {
-      "id": self.id, 
-      "user": User.query.filter_by(id=self.user).first().serialize_profile(),
-      "time": self.time, 
-      "comments": [c.serialize() for c in self.comments], 
-      "title": self.title, 
-      "contents": self.contents
-    }
+    def serialize_profile(self):
+        """
+        Serializes the profile of the user. This includes id, name, university, grade
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "university": self.university,
+            "grade": self.grade
+        }
+
+    def serialize_friends(self):
+        """Serializes the profiles of the user's friends"""
+        return {
+            "id": self.id,
+            "friends": [f.serialize_profile() for f in self.friends]
+        }
+
+    def serialize_posts(self):
+        """
+        Serializes the posts of the user.
+        """
+        return {
+            "id": self.id,
+            "posts": [p.serialize() for p in self.posts]
+        }
+
+    def serialize_game(self):
+        """
+        Serializes game information of the user
+        """
+        return {
+            "id": self.id,
+            "target_of_card": [t.serialize() for t in self.target_of_card],
+            "target_card": [t.serialize() for t in self.target_card],
+            "game": Game.query.filter_by(id=self.game).first().serialize_info(),
+            "stats": Stats.query.filter_by(id=self.stats).first().serialize()
+        }
+
+    # def init_stats(self):
+    #     """Initializes the stats id to match user id"""
+    #     self.stats = self.id
 
 
+class Game(db.Model):
+    __tablename__ = "game"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # Include a leaderboard
+    name = db.Column(db.String, default="none", nullable=False)
+    users = db.relationship("User", cascade="delete")
+    is_private = db.Column(db.Boolean, nullable=False)
+    cards = db.relationship("Card", cascade="delete")
 
-class Comment (db.Model): 
-  __tablename__ = "comment"
-  id = db.Column (db.Integer, primary_key = True, autoincrement=True)
-  user = db.Column (db.Integer, db.ForeignKey("user.id"), nullable=False)
-  time = db.Column (db.String, nullable= False)
-  post = db.Column (db.Integer, db.ForeignKey("post.id"), nullable = False)
-  contents = db.Column (db.String)
+    def serialize_info(self):
+        """
+        Serializes basic information of the card (id, name, privacy)
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "is_private": self.is_private
+        }
 
-  def serialize(self): 
-    """
-    Serializes the contents of the comment
-    """
-    return{
-      "id": self.id, 
-      "user": User.query.filter_by(id=self.user).first().serialize_profile(),
-      "time": self.time,
-      "contents": self.contents
-    }
+    def init_public_game():
+        """Initializes a public game that will have id=0"""
+        new_game = Game(
+            name="public default",
+            users=[],
+            is_private=False,
+            cards=[]
+        )
+        db.session.add(new_game)
+        db.session.flush()
+        db.session.commit()
 
 
-class Stats (db.Model): 
-  __tablename__ = "stats"
-  id = db.Column (db.Integer, primary_key = True, autoincrement=True)
-  total = db.Column (db.Integer)
+class Card(db.Model):
+    __tablename__ = "card"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    target = db.relationship(
+        "User", secondary=target_assoc, back_populates="target_card")
+    belong = db.relationship(
+        "User", secondary=target_of_assoc, back_populates="target_of_card")
+    game = db.Column(db.Integer, db.ForeignKey("game.id"), nullable=False)
 
-  def serialize(self):
-    """
-    Serializes the stats of the user
-    """
-    return{
-      "id": self.id, 
-      "total": self.total
-    }
+    def serialize(self):
+        """
+        Serializes the contents of the card
+        """
+        return {
+            "id": self.id,
+            "target": User.query.filter_by(id=self.target).first().serialize_profile(),
+            "belong": [u.serialize_profile() for u in self.belong],
+            "game": Game.query.filter_by(id=self.game).first().serialize_info()
+        }
 
+
+class Post (db.Model):
+    __tablename__ = "post"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    time = db.Column(
+        db.String, default="")
+    comments = db.relationship("Comment", cascade="delete")
+    title = db.Column(db.String, default="no title")
+    contents = db.Column(db.String, default="no content")
+
+    def serialize(self):
+        """
+        Serializes the contents of the post
+        """
+        return {
+            "id": self.id,
+            "user": User.query.filter_by(id=self.user).first().serialize_profile(),
+            "time": self.time,
+            "comments": [c.serialize() for c in self.comments],
+            "title": self.title,
+            "contents": self.contents
+        }
+
+
+class Comment (db.Model):
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    time = db.Column(
+        db.String, default="")
+    post = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+    contents = db.Column(db.String)
+
+    def serialize(self):
+        """
+        Serializes the contents of the comment
+        """
+        return {
+            "id": self.id,
+            "user": User.query.filter_by(id=self.user).first().serialize_profile(),
+            "time": self.time,
+            "contents": self.contents
+        }
+
+
+class Stats (db.Model):
+    __tablename__ = "stats"
+    id = db.Column(db.Integer, primary_key=True)
+    total = db.Column(db.Integer, default=0)
+
+    def serialize(self):
+        """
+        Serializes the stats of the user
+        """
+        return {
+            "id": self.id,
+            "total": self.total
+        }
+
+    def init_stats():
+        """Initializes the stats for a first time user"""
+        new_stats = Stats(
+            total=0
+        )
+        db.session.add(new_stats)
+        db.session.flush()
+        db.session.commit()
+        return new_stats.id
